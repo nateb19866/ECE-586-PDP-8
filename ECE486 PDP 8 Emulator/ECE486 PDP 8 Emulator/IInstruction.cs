2d7a1cf12f2c4b29b8f5f1c4929309f98b9655d4
nateb19866 <nbecker@pdx.edu>
2015-01-11 00:24:31 -0800

    Created Initial Project

---
+++ ECE486 PDP 8 Emulator/ECE486 PDP 8 Emulator/IInstruction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECE486_PDP_8_Emulator
+{
+    interface IInstruction
+    {
+    }
+}
