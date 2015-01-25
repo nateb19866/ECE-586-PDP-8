140db26e321dcbf744dfbfae32030b63160b3ce5
nateb19866 <nbecker@pdx.edu>
2015-01-25 15:44:35 -0800

    Added example code to the LoaderUnitTest class

--- Src/ECE486 PDP 8 Emulator Tests/LoaderUnitTests.cs
+++ Src/ECE486 PDP 8 Emulator Tests/LoaderUnitTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ECE486_PDP_8_Emulator;
+using ECE486_PDP_8_Emulator.Classes;
 
 namespace ECE486_PDP_8_Emulator_Tests
 {
@@ -17,7 +18,15 @@ namespace ECE486_PDP_8_Emulator_Tests
 
             MemArray FinalMemArray = new MemArray(FinalArray, "MemTrace.tr");
 
-            MemArray TestMemArray = BinLoader.LoadFile(".\\TestFile.bin");
+
+            LoaderResult FinalLoaderRslt = new LoaderResult()
+            {
+                FinishedArray = FinalMemArray,
+                FirstInstructionAddress = 200
+
+            };
+
+            LoaderResult TestMemArray = BinLoader.LoadFile(".\\TestFile.bin", ".\\Trace.tr");
 
             Assert.AreEqual(FinalMemArray,TestMemArray);
 
