bcfd9fafd07e57832a76026f6dfe2128a403d099
KathyBui14 <buikt@pdx.edu>
2015-02-16 12:19:10 -0800

    Added Source Skeleton for uInstr 3 set.

---
+++ Src/ECE486 PDP 8 Emulator/Instructions/CamInstruction.cs
@@ -0,0 +1,54 @@
+﻿using ECE486_PDP_8_Emulator.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECE486_PDP_8_Emulator.Instructions
+{
+    public class CamInstruction : IInstruction
+    {
+
+        private int ClockCycles = 1;
+        private Constants.Microcode InstructionType = Constants.Microcode.CAM;
+
+
+        public InstructionResult ExecuteInstruction(InstructionItems instItems)
+        {
+            // Need to implement clear both AC and MQ registers ( CLA and MQL )
+
+            return new InstructionResult()
+            {
+                accumulatorOctal = instItems.accumulatorOctal,
+                LinkBit = instItems.LinkBit,
+                MemoryAddress = instItems.MemoryAddress,
+                MemoryValueOctal = instItems.MemoryValueOctal,
+                MicroCodes = instItems.MicroCodes,
+                BranchTaken = false,
+                pcCounter = instItems.pcCounter++,
+                SetMemValue = false
+            };
+
+
+        }
+
+
+
+
+        public int clockCycles
+        {
+            get
+            {
+                return ClockCycles;
+            }
+
+        }
+
+
+        public Constants.Microcode instructionType
+        {
+            get { return InstructionType; }
+        }
+    }
+}
