0df7e7593875e1a58be722d291ed0b18fb2ceff2
KathyBui14 <buikt@pdx.edu>
2015-02-26 20:20:29 -0800

    Fixed RTR instruction to correctly rotate left twice.

--- Src/ECE486 PDP 8 Emulator/Instructions/OprInstruction.cs
+++ Src/ECE486 PDP 8 Emulator/Instructions/OprInstruction.cs
@@ -362,6 +362,10 @@ namespace ECE486_PDP_8_Emulator.Instructions
 
 
         }
+
+        /* 
+        * RAL Instruction Rotates the 13 bit Link and AC right twice.
+        */
         public InstructionResult RTLInstruction(InstructionItems instItems)
         {
             int tempLink = 0;
@@ -369,20 +373,22 @@ namespace ECE486_PDP_8_Emulator.Instructions
             //Rotate 13 bit Link/AC left by 2 ( 2 of RAL )
 
             //Converting to byte array to make things easier.
-            Int16 TestWord1Bytes = Convert.ToInt16(instItems.accumulatorOctal.ToString(), 12);
+            Int16 TestWord1Bytes = Convert.ToInt16(instItems.accumulatorOctal.ToString(), 8);
 
-            // Concatenante Linkbit and AC
+            // Put Link Bit into 13th bit of AC
             if (instItems.LinkBit == true)
-                tempLink = 1;
-            else // =0
-                tempLink = 0;
+                TestWord1Bytes |= (1 << 12);
+            else // Link bit false = 0
+                TestWord1Bytes &= ~(1 << 12);
 
             // Rotate 13 Bit Link/AC right by 1
-            int FinalWord = (tempLink + TestWord1Bytes) << 2;
 
-            int finalAC = Convert.ToInt16(FinalWord.ToString(), 12);
-            tempLink = Convert.ToInt16(FinalWord.ToString().Remove(12, 1), 1); // return 13th bit
+            // AND with mask to get 12 bits with rotate right once
+            int finalAC = ((TestWord1Bytes << 2) | (TestWord1Bytes >> 11)) & 0xfff;
+            // AND with mask to get 13th bit with rotate right once
+            tempLink = ((((TestWord1Bytes << 2) | (TestWord1Bytes >> 11)) >> 12) & 1);
 
+            // Set Link Bit to bool accordingly
             if (tempLink == 0)
                 LinkReturn = false;
             else
