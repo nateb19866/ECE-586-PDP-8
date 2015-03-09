3cbbb3399e0abc2faeb45c35420c955c490ba32f
KathyBui14 <buikt@pdx.edu>
2015-03-09 00:02:56 -0700

    Added testcase for objectloader unit. tested and passed.

--- Src/ECE486 PDP 8 Emulator Tests/LoaderUnitTests.cs
+++ Src/ECE486 PDP 8 Emulator Tests/LoaderUnitTests.cs
@@ -33,6 +33,38 @@ namespace ECE486_PDP_8_Emulator_Tests
 
         }
 
+        [TestMethod]
+        public void Test2ndObjLoading()
+        {
+
+            ILoader FileLoader = new ObjLoader();
+            LoaderResult TestMemArray = FileLoader.LoadFile(Resources.TestFilePath + "/intSum.obj");
+
+            //Instruction section - 200 octal = 0x80 Hex
+            Assert.AreEqual(Convert.ToInt32(7200.ToString(), 8), TestMemArray.FinishedArray.GetValue(0x80, false, false));
+            Assert.AreEqual(Convert.ToInt32(3250.ToString(), 8), TestMemArray.FinishedArray.GetValue(0x81, false, false));
+            Assert.AreEqual(Convert.ToInt32(1252.ToString(), 8), TestMemArray.FinishedArray.GetValue(0x82, false, false));
+            Assert.AreEqual(Convert.ToInt32(3251.ToString(), 8), TestMemArray.FinishedArray.GetValue(0x83, false, false));
+            Assert.AreEqual(Convert.ToInt32(1250.ToString(), 8), TestMemArray.FinishedArray.GetValue(0x84, false, false));
+            Assert.AreEqual(Convert.ToInt32(1251.ToString(), 8), TestMemArray.FinishedArray.GetValue(0x85, false, false));
+           
+            Assert.AreEqual(Convert.ToInt32(3250.ToString(), 8), TestMemArray.FinishedArray.GetValue(0x86, false, false));
+            Assert.AreEqual(Convert.ToInt32(7240.ToString(), 8), TestMemArray.FinishedArray.GetValue(0x87, false, false));
+            //test for octal increment from 207 to 210
+            Assert.AreEqual(Convert.ToInt32(1251.ToString(), 8), TestMemArray.FinishedArray.GetValue(0x88, false, false));
+            Assert.AreEqual(Convert.ToInt32(7440.ToString(), 8), TestMemArray.FinishedArray.GetValue(0x89, false, false));
+            //test for octal increment from 211 to 212
+            Assert.AreEqual(Convert.ToInt32(5203.ToString(), 8), TestMemArray.FinishedArray.GetValue(0x8A, false, false));
+            Assert.AreEqual(Convert.ToInt32(7402.ToString(), 8), TestMemArray.FinishedArray.GetValue(0x8B, false, false));
+
+
+            //Data section - 0250 octal (000 010 101 000) = 0x0A8(0000 1010 1000) hex
+            Assert.AreEqual(Convert.ToInt32(2.ToString(), 8), TestMemArray.FinishedArray.GetValue(0xA8, false, false));
+            Assert.AreEqual(Convert.ToInt32(3.ToString(), 8), TestMemArray.FinishedArray.GetValue(0xA9, false, false));
+            Assert.AreEqual(Convert.ToInt32(0.ToString(), 8), TestMemArray.FinishedArray.GetValue(0xAA, false, false));
+
+        }
+
 
         [TestMethod]
         public void TestBlankStartAddress()
